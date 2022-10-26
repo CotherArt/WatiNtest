5843060850c0d7993e61617d10245d0c5c2e348e
CotherArt <64714914+CotherArt@users.noreply.github.com>
2022-10-25 23:02:13 -0600

    Add project files.

---
+++ WatiNtest/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WatiNtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "button1";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(12, 41);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(316, 257);
+            this.tbConsole.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 316);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "WatiN test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbConsole;
+    }
+}
+
---
+++ WatiNtest/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WatiN.Core;
+using WatiN.Core.Native.InternetExplorer;
+using WatiN.Core.Native.Mozilla;
+
+namespace WatiNtest
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        private IE browser = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Inicializar el explorador web
+            browser = new IE();
+            tbConsole.Text = "Empezando...";
+            //Ir al sitio web deseado
+            browser.GoTo("http://www.google.com");
+            //Localizar el text box de google y llenarlo
+            browser.TextField(Find.ByName("q")).TypeText("TarritoAzul.com");
+            //Hacer click en el boton de buscar
+            browser.Element(Find.ByName("btnG")).Click();
+            browser.WaitForComplete();
+            tbConsole.AppendText("Fin.");
+        }
+    }
+}
